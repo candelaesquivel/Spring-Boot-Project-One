d33e7de90024b3cdfb6fb2350ba0beeec580f4aa
Candela Esquivel <80803466+candelaesquivel@users.noreply.github.com>
2023-01-26 16:42:33 -0300

    Datasource configuration done

---
+++ src/main/java/com/springBoot/Entity/User.java
@@ -0,0 +1,90 @@
+package com.springBoot.Entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_user", nullable = false ,unique = true)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(length = 50)
+    private LocalDate birthDay;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Post> posts= new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String name, String email, LocalDate birthDay) {
+        this.name = name;
+        this.email = email;
+        this.birthDay = birthDay;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDay=" + birthDay +
+                ", posts=" + posts +
+                '}';
+    }
+}
--- src/main/java/com/springBoot/configuration/GeneralConfiguration.java
+++ src/main/java/com/springBoot/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.springBoot.Bean.MyBeanWithPropiertiesImplement;
 import com.springBoot.Pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
 @EnableConfigurationProperties(UserPojo.class)
 @Configuration
 public class GeneralConfiguration {
@@ -22,4 +25,15 @@ public class GeneralConfiguration {
     public MyBeanWithPropierties myBean(){
         return new MyBeanWithPropiertiesImplement(name, lastName);
     }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testDB");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+
 }
